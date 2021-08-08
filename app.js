9161dee7e7336084ca0cc318883eaeb8128e4e1e
ammarzeyad <ammarramadan16@yahoo.com>
2021-08-09 02:58:52 +0300

    lab06

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ let seattle = {
   maxNumber:65,
   avgCookie:6.3,
   soldCoockiesPerHour:[],
-  total:0,
   pTotal: function () {
     let message = '';
+    let total =0;
     for(let i =0; i<hoursWork.length;i++){
-      message = message + `\n ${hoursWork[i]}: ${this.soldCookiesPerHour[i]} cookies`;
-      this.total = this.total+ this.soldCookiesPerHour[i];
+      message += `\n${hoursWork[i]}: ${this.soldCoockiesPerHour[i]} cookies`;
+      // total = total + this.soldCookiesPerHour[i];
+      console.log(this.soldCoockiesPerHour[i]);
     }
-    message = message+ `\n total: ${this.total}`;
+    message = message + `\n total: ${this.total}`;
     console.log(message);
   },
   getRandome: function () {
-    for(let i =0; i<hoursWork;i++)
-      this.minNumber = Math.ceil(this.minNumber);
-    this.maxNumber = Math.floor(this.maxNumber);
-    let rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber) + this.minNumber);
-    this.soldCoockiesPerHour.push(rand);
+    let rand;
+    for(let i =0; i<hoursWork.length;i++){
+      rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber + 1) + this.minNumber);
+      this.soldCoockiesPerHour.push(rand);
+    }
   }
 };
 seattle.getRandome();
@@ -33,27 +34,27 @@ let tokyo = {
   maxNumber:24,
   avgCookie:1.2,
   soldCoockiesPerHour:[],
-  total:0,
   pTotal: function () {
     let message = '';
+    let total =0;
     for(let i =0; i<hoursWork.length;i++){
-      message = message + `\n ${hoursWork[i]}: ${this.soldCookiesPerHour[i]} cookies`;
-      this.total = this.total+ this.soldCookiesPerHour[i];
+      message += `\n${hoursWork[i]}: ${this.soldCoockiesPerHour[i]} cookies`;
+      // total = total + this.soldCookiesPerHour[i];
+      console.log(this.soldCoockiesPerHour[i]);
+
     }
     message = message+ `\n total: ${this.total}`;
     console.log(message);
   },
   getRandome: function () {
-    for(let i =0; i<hoursWork;i++)
-      this.minNumber = Math.ceil(this.minNumber);
-    this.maxNumber = Math.floor(this.maxNumber);
-    let rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber) + this.minNumber);
-    this.soldCoockiesPerHour.push(rand);
-  }
+    let rand;
+    for(let i =0; i<hoursWork.length;i++){
+      rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber + 1) + this.minNumber);
+      this.soldCoockiesPerHour.push(rand);
+    }}
 };
 tokyo.getRandome();
 tokyo.pTotal();
-
 
 let dubai = {
   name: 'dubai',
@@ -61,80 +62,80 @@ let dubai = {
   maxNumber:38,
   avgCookie:3.7,
   soldCoockiesPerHour:[],
-  total:0,
   pTotal: function () {
     let message = '';
+    let total =0;
     for(let i =0; i<hoursWork.length;i++){
-      message = message + `\n ${hoursWork[i]}: ${this.soldCookiesPerHour[i]} cookies`;
-      this.total = this.total+ this.soldCookiesPerHour[i];
+      message += `\n${hoursWork[i]}: ${this.soldCoockiesPerHour[i]} cookies`;
+      // total = total + this.soldCookiesPerHour[i];
+      console.log(this.soldCoockiesPerHour[i]);
+
     }
     message = message+ `\n total: ${this.total}`;
     console.log(message);
   },
   getRandome: function () {
-    for(let i =0; i<hoursWork;i++)
-      this.minNumber = Math.ceil(this.minNumber);
-    this.maxNumber = Math.floor(this.maxNumber);
-    let rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber) + this.minNumber);
-    this.soldCoockiesPerHour.push(rand);
-  }
+    let rand;
+    for(let i =0; i<hoursWork.length;i++){
+      rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber + 1) + this.minNumber);
+      this.soldCoockiesPerHour.push(rand);
+    }}
 };
 dubai.getRandome();
 dubai.pTotal();
 
-
 let paris = {
   name: 'paris',
-  minNumber:20,
+  minNumber:11,
   maxNumber:38,
-  avgCookie:2.3,
+  avgCookie:3.7,
   soldCoockiesPerHour:[],
-  total:0,
   pTotal: function () {
     let message = '';
+    let total =0;
     for(let i =0; i<hoursWork.length;i++){
-      message = message + `\n ${hoursWork[i]}: ${this.soldCookiesPerHour[i]} cookies`;
-      this.total = this.total+ this.soldCookiesPerHour[i];
+      message += `\n${hoursWork[i]}: ${this.soldCoockiesPerHour[i]} cookies`;
+      // total = total + this.soldCookiesPerHour[i];
+      console.log(this.soldCoockiesPerHour[i]);
+
     }
     message = message+ `\n total: ${this.total}`;
     console.log(message);
   },
   getRandome: function () {
-    for(let i =0; i<hoursWork;i++)
-      this.minNumber = Math.ceil(this.minNumber);
-    this.maxNumber = Math.floor(this.maxNumber);
-    let rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber) + this.minNumber);
-    this.soldCoockiesPerHour.push(rand);
-  }
+    let rand;
+    for(let i =0; i<hoursWork.length;i++){
+      rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber + 1) + this.minNumber);
+      this.soldCoockiesPerHour.push(rand);
+    }}
 };
-
 paris.getRandome();
 paris.pTotal();
 
 let lima = {
   name: 'lima',
-  minNumber:2,
-  maxNumber:16,
-  avgCookie:4.6,
+  minNumber:11,
+  maxNumber:38,
+  avgCookie:3.7,
   soldCoockiesPerHour:[],
-  total:0,
   pTotal: function () {
     let message = '';
+    let total =0;
     for(let i =0; i<hoursWork.length;i++){
-      message = message + `\n ${hoursWork[i]}: ${this.soldCookiesPerHour[i]} cookies`;
-      this.total = this.total+ this.soldCookiesPerHour[i];
+      message += `\n${hoursWork[i]}: ${this.soldCoockiesPerHour[i]} cookies`;
+      // total = total + this.soldCookiesPerHour[i];
+      console.log(this.soldCoockiesPerHour[i]);
+
     }
     message = message+ `\n total: ${this.total}`;
     console.log(message);
   },
   getRandome: function () {
-    for(let i =0; i<hoursWork;i++)
-      this.minNumber = Math.ceil(this.minNumber);
-    this.maxNumber = Math.floor(this.maxNumber);
-    let rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber) + this.minNumber);
-    this.soldCoockiesPerHour.push(rand);
-  },
+    let rand;
+    for(let i =0; i<hoursWork.length;i++){
+      rand = Math.floor(Math.random() * (this.maxNumber - this.minNumber + 1) + this.minNumber);
+      this.soldCoockiesPerHour.push(rand);
+    }}
 };
 lima.getRandome();
 lima.pTotal();
-
